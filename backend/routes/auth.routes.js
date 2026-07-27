0f1e9fd0538a3c716c8e3609ea83c04ca21d1956
om-sairaj-rout <omsairajraut@gmail.com>
2026-07-27 22:11:00 +0530

    minimal change

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,45 +1,37 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
 
-const RegisterController = require('../controllers/authControllers/userRegister.controllers.js');
-const LoginController = require('../controllers/authControllers/userLogin.controllers.js');
-const LogoutController = require('../controllers/authControllers/userLogout.controllers.js');
-const authCheckController = require('../controllers/authControllers/authCheck.controllers.js');
-const { checkAuth, authRoles } = require('../middlewares/auth.middleware.js');
-const forgotPassword = require('../controllers/authControllers/forgotPassword.controllers.js');
-const resetPassword = require('../controllers/authControllers/resetPassword.controllers.js');
-const getAllUsers =
-require('../controllers/authControllers/getAllUsers.controllers');
+const RegisterController = require("../controllers/authControllers/userRegister.controllers.js");
+const LoginController = require("../controllers/authControllers/userLogin.controllers.js");
+const LogoutController = require("../controllers/authControllers/userLogout.controllers.js");
+const authCheckController = require("../controllers/authControllers/authCheck.controllers.js");
+const { checkAuth, authRoles } = require("../middlewares/auth.middleware.js");
+const forgotPassword = require("../controllers/authControllers/forgotPassword.controllers.js");
+const resetPassword = require("../controllers/authControllers/resetPassword.controllers.js");
+const getAllUsers = require("../controllers/authControllers/getAllUsers.controllers");
 
-const updateUserController =
-require('../controllers/authControllers/updateUser.controllers');
-const deleteUserController =
-require('../controllers/authControllers/deleteUser.controllers');
+const updateUserController = require("../controllers/authControllers/updateUser.controllers");
+const deleteUserController = require("../controllers/authControllers/deleteUser.controllers");
 
-authRouter.post('/user/register', RegisterController);
-authRouter.post('/user/login', LoginController);
-authRouter.post('/user/logout', LogoutController);
-authRouter.get('/auth/check', checkAuth, authCheckController);
-authRouter.post("/forgot-password",forgotPassword);
-authRouter.post("/reset-password/:token",resetPassword);
-authRouter.get(
-  "/users",
-  checkAuth,
-  authRoles("admin"),
-  getAllUsers
-);
+authRouter.post("/user/register", RegisterController);
+authRouter.post("/user/login", LoginController);
+authRouter.post("/user/logout", LogoutController);
+authRouter.get("/auth/check", checkAuth, authCheckController);
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/reset-password/:token", resetPassword);
+authRouter.get("/users", checkAuth, authRoles("admin"), getAllUsers);
 
 authRouter.put(
   "/update-user/:id",
   checkAuth,
   authRoles("admin"),
-  updateUserController
+  updateUserController,
 );
 authRouter.delete(
   "/delete-user/:id",
   checkAuth,
   authRoles("admin"),
-  deleteUserController
+  deleteUserController,
 );
 
 module.exports = authRouter;
