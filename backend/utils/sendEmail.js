b88338f9e076da7745d0e2184ea4354d2de7262e
om-sairaj-rout <omsairajraut@gmail.com>
2026-08-04 02:14:00 +0530

    bug fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
